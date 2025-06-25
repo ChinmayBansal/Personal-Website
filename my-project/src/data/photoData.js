17332740ee5bfdfb5c8dfa0d7913076dc22bf209
Chinmay Bansal <chinmaybansalsc@outlook.com>
2025-06-25 12:59:40 -0700

    empty

--- my-project/src/data/photoData.js
+++ my-project/src/data/photoData.js
@@ -3,48 +3,4 @@
 export const photoData = [
   // Temporarily empty to prevent loading errors
   // Add your photos back once hosting is working
-  /*
-  {
-    id: 1,
-    src: '/photos/photo1.jpg', 
-    title: 'Photo 1',
-    description: 'Add your photo description here',
-    isFavorite: true
-  },
-  {
-    id: 2,
-    src: '/photos/photo2.jpg', 
-    title: 'Photo 2', 
-    description: 'Add your photo description here',
-    isFavorite: false
-  },
-  {
-    id: 3,
-    src: '/photos/photo3.jpg', 
-    title: 'Photo 3',
-    description: 'Add your photo description here',
-    isFavorite: true
-  },
-  {
-    id: 4,
-    src: '/photos/photo4.jpg', 
-    title: 'Photo 4',
-    description: 'Add your photo description here',
-    isFavorite: true
-  },
-  {
-    id: 5,
-    src: '/photos/photo5.jpg',
-    title: 'Photo 5',
-    description: 'Add your photo description here',
-    isFavorite: false
-  },
-  {
-    id: 6,
-    src: '/photos/photo6.jpg',
-    title: 'Photo 6',
-    description: 'Add your photo description here',
-    isFavorite: true
-  },
-  
 ];
